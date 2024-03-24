e5d620943ad3a2c4b450a1ccdf8aa28d40b178f7
eg337 <eg6322@gmail.com>
2024-03-24 05:31:03 -0400

    connected front and backend badly

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, Response
+from pikachu_finder import source_image
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -8,10 +8,11 @@ def home():
         return render_template('index.html')
 
     link = request.form.get('link')
-
+    artist = source_image(link)
     print(link)
+    print(artist)
 
-    return render_template('index.html')
+    return Response(artist, status = 200, mimetype ="text/html")
 
 if __name__ == '__main__':
     app.run()
--- pikachu_finder.py
+++ pikachu_finder.py
@@ -6,79 +6,82 @@ from selenium import webdriver
 from urllib.request import urlopen 
 import json 
 
-params = {
-  "engine": "google_reverse_image",
-  "image_url": "https://pbs.twimg.com/media/GG_Zyf6aIAAzyJj?format=png&name=900x900",
-  "api_key": "852564d05052e29ed6b66ab33277484004f9dd4e4b41aec7d4ff7d1c3e309497",
-  "filter": 0
-}
+def source_image(image_url):
+  print(image_url)
 
-search = GoogleSearch(params)
-results = search.get_dict()
-inline_images = results["inline_images"]
+  params = {
+    "engine": "google_reverse_image",
+    "image_url": image_url,
+    "api_key": "852564d05052e29ed6b66ab33277484004f9dd4e4b41aec7d4ff7d1c3e309497",
+    "filter": 0
+  }
 
-
-print(results)
-print(len(results))
-
-print(inline_images)
-image_sizes = results["image_sizes"]
-print(image_sizes)
-dic = image_sizes[0]
+  search = GoogleSearch(params)
+  results = search.get_dict()
+  print(results)
+  #inline_images = results["inline_images"]
 
 
+  print(results)
+  print(len(results))
 
-url = image_sizes[0]['serpapi_link']
-url += "&api_key="
-url += params['api_key']
-print(url)
+  #print(inline_images)
+  image_sizes = results["image_sizes"]
+  print(image_sizes)
+  dic = image_sizes[0]
 
 
 
-response  = urlopen(url)
-
-data_json = json.loads(response.read()) 
-  
-# print the json response 
-print(data_json) 
-
-images = data_json["images_results"]
-
-links = []
-for i in images:
-    links.append(i["link"])
-links = links[::-1]
-#((artist|credit( to)?|twt|twitter|twi|ig|instagram|insta|op|facebook|fb|pixiv):?\s*@?([^\s])+
+  url = image_sizes[0]['serpapi_link']
+  url += "&api_key="
+  url += params['api_key']
+  print(url)
 
 
-#regex = "((artist|owner|credit( to)?|twt|twitter|twi|ig|instagram|insta|op|facebook|fb|pixiv):?\s*@?([^\s])+)"
-regex = "((artist|owner|credit( to)?|twt|twitter|twi|ig|instagram|insta|op|facebook|fb|pixiv):?\s*@?([^\s]+))+"
-driver = webdriver.Chrome()
 
-texts = []
-matches = []
-for i in links:
-    driver.get(i)
-    data = driver.page_source
-    soup = BeautifulSoup(data)
-    txt = soup.prettify()
-    texts.append(txt)
-    x = re.findall(regex, txt)
-    matches.append(x)
+  response  = urlopen(url)
 
-print(matches)
-for i in range(len(links)):
-    print(i)
-    print(links[i])
-print(texts[0])
-regex = "(\s(artist|owner|credit( to)?|twt|twitter|twi|ig|instagram|insta|op|facebook|fb|pixiv):?\s+@?([\w_]+))+"
-#made there be a space at the start of the word bc of things like "top: 0"
-#changed the artists name from non white space chars to alphanumeric plus underscores, bc of config = ...
-#
-for i in range(len(texts)):
-    x = re.findall(regex, texts[i])
-    print(i)
-    if len(x) > 0:
-        print(x)
-        print(x[0][3])
-        break
+  data_json = json.loads(response.read()) 
+    
+  # print the json response 
+  print(data_json) 
+
+  images = data_json["images_results"]
+
+  links = []
+  for i in images:
+      links.append(i["link"])
+  links = links[::-1]
+  #((artist|credit( to)?|twt|twitter|twi|ig|instagram|insta|op|facebook|fb|pixiv):?\s*@?([^\s])+
+
+
+  #regex = "((artist|owner|credit( to)?|twt|twitter|twi|ig|instagram|insta|op|facebook|fb|pixiv):?\s*@?([^\s])+)"
+  regex = "((artist|owner|credit( to)?|twt|twitter|twi|ig|instagram|insta|op|facebook|fb|pixiv):?\s*@?([^\s]+))+"
+  driver = webdriver.Chrome()
+
+  texts = []
+  matches = []
+  for i in links:
+      driver.get(i)
+      data = driver.page_source
+      soup = BeautifulSoup(data)
+      txt = soup.prettify()
+      texts.append(txt)
+      x = re.findall(regex, txt)
+      matches.append(x)
+
+  print(matches)
+  for i in range(len(links)):
+      print(i)
+      print(links[i])
+  print(texts[0])
+  regex = "(\s(artist|owner|credit( to)?|twt|twitter|twi|ig|instagram|insta|op|facebook|fb|pixiv):?\s+@?([\w_]+))+"
+  #made there be a space at the start of the word bc of things like "top: 0"
+  #changed the artists name from non white space chars to alphanumeric plus underscores, bc of config = ...
+  #
+  for i in range(len(texts)):
+      x = re.findall(regex, texts[i])
+      print(i)
+      if len(x) > 0:
+          print(x)
+          return x[0][3]
